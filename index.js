ad50b06a805b684201c3a2bf76b747dfd760b826
mixassio <mixail.bondarev@gmail.com>
2018-11-12 13:52:10 +0300

    second bot

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const Telegraf = require('telegraf')
+var TelegramBot = require('node-telegram-bot-api');
 
-const app = new Telegraf('728024267:AAHSCrzaKxxncCrUfzrZnu67qtvjZDcp0EM')
-app.hears('hi', ctx => {
-    return ctx.reply('Hey!');
-   });
-   
-app.startPolling();
+var token = 'ТУТ_ВСТАВЛЯЕМ_ТОКЕН';
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
