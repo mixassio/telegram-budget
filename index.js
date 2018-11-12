759fb35888beac788ca608b686674f3bcb91da5c
mixassio <mixail.bondarev@gmail.com>
2018-11-12 14:06:12 +0300

    h

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var TelegramBot = require('node-telegram-bot-api');
 
-var token = 'ТУТ_ВСТАВЛЯЕМ_ТОКЕН';
+var token = '728024267:AAHSCrzaKxxncCrUfzrZnu67qtvjZDcp0EM';
 var bot = new TelegramBot(token, {polling: true});
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
-  
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-  });
+
+var notes = [];
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++){
+        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+            if ( notes[i]['time'] == curDate ) {
+                bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+                notes.splice(i,1);
+            }
+        }
+},1000);
