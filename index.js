6aea2f579620919719c3d33b0f2496785230293b
mixassio <mixail.bondarev@gmail.com>
2018-11-12 14:59:09 +0300

    fix

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
     notes.push({ 'uid': userId, 'time': time, 'text': text });
 
     bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++){
-        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-            if ( notes[i]['time'] == curDate ) {
-                bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-                notes.splice(i,1);
+    setInterval(function(){
+        for (var i = 0; i < notes.length; i++){
+            var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+                if ( notes[i]['time'] == curDate ) {
+                    bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+                    notes.splice(i,1);
+                }
             }
-        }
-},1000);
+    },1000);
+});
