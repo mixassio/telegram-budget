64404dbff5677ae1f39b66f43152bd33e3e135bd
mixassio <mixail.bondarev@gmail.com>
2018-12-05 19:26:08 +0300

    f

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ let users = {
         otherUser: 276986665,
     }
 }
-
+setInterval(() => {
+    console.log(new Date().toString());
+    if (new Date().getHours() === 4) {
+        users[276986665].current += dayLimit;
+        users[378986745].current += dayLimit;
+        const message = `Доброе утро, лимит увеличен на ${dayLimit} рублей.\n Бюджет на сегодня:\n ${users[276986665].name}: ${users[276986665].current} рублей.\n ${users[378986745].name}: ${users[378986745].current} рублей.`;
+        bot.sendMessage(276986665, message);
+        bot.sendMessage(378986745, message);
+    }
+}, 60000);
 bot.onText(/(.+) (\d+)/, function (msg, match) {
     const userId = msg.from.id;
     if (users[userId]) {
@@ -36,16 +45,5 @@ bot.onText(/(.+) (\d+)/, function (msg, match) {
     } else {
         bot.sendMessage(userId, 'Вы не из нашей семьи, проваливайте!');
     }
-
-    setInterval(() => {
-        console.log(new Date().toString());
-        if (new Date().getHours() === 4) {
-            users[276986665].current += dayLimit;
-            users[378986745].current += dayLimit;
-            const message = `Доброе утро, лимит увеличен на ${dayLimit} рублей.\n Бюджет на сегодня:\n ${users[276986665].name}: ${users[276986665].current} рублей.\n ${users[378986745].name}: ${users[378986745].current} рублей.`;
-            bot.sendMessage(276986665, message);
-            bot.sendMessage(378986745, message);
-        }
-    }, 60000);
 
 });
